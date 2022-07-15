e28a3448ed2f3b1c64c2458a0485243b59ae729c
Agustin Gonzalez <agustin-ag@live.com>
2022-07-15 17:52:54 +0300

    Added items that forgot to commit

--- tests/users.test.ts
+++ tests/users.test.ts
@@ -3,6 +3,54 @@ import app from '../src/app'
 
 const api = supertest(app)
 
+test('user creation without username fails', async () => {
+  const newUser= {
+    password: 'password'
+  }
+  await api.post('/users')
+    .send(newUser)
+    .expect(400)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('user creation without password fails', async () => {
+  const newUser= {
+    username: 'username'
+  }
+  await api.post('/users')
+    .send(newUser)
+    .expect(400)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('user creation with a noString username fails', async () => {
+  const newUser= {
+    username: {},
+    password: 'password'
+  }
+  await api.post('/users')
+    .send(newUser)
+    .expect(400)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('user creation with a noString password fails', async () => {
+  const newUser= { username: 'username', password: {}
+  }
+  await api.post('/users')
+    .send(newUser)
+    .expect(400)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('user creation with a an array body fails', async () => {
+  const newUser: unknown[] = []
+  await api.post('/users')
+    .send(newUser)
+    .expect(400)
+    .expect('Content-Type', /application\/json/)
+})
+
 test('user creation succeeds', async () => {
   const newUser = {
     username: 'username',
